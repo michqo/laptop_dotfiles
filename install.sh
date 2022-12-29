8f4e01faae1e4796115b7af795ac966e996984a3
MichalUSER <42279279+MichalUSER@users.noreply.github.com>
2022-12-29 21:56:02 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Delete
+rm -rf ~/.bin
+rm -rf ~/.config/fish
+rm -rf ~/.config/gitui
+rm -rf ~/.config/nvim
+rm -rf ~/.config/kitty
+rm -rf ~/.tmux.conf
+
+# Create links
 ln -sf ~/dotfiles/.bin ~/.bin
 ln -sf ~/dotfiles/.config/fish ~/.config/fish
 ln -sf ~/dotfiles/.config/gitui ~/.config/gitui
